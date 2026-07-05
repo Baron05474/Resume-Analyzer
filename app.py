83b910fcc6739becb767327b5ee5b637e97e1bf4
Baron Bhowmick <baronbhowmickbaron@gmail.com>
2026-07-05 22:20:36 +0600

    fix port routing for render

--- app.py
+++ app.py
@@ -224,4 +224,6 @@ def analyze():
     return jsonify({"message": "Only PDF files are allowed."}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
